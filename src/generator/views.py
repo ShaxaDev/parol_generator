c0c9144760a5e5847450891b9b35fe9ebe9ae989
shaxzod <shaxzadjanaka@gmail.com>
2021-02-13 09:35:22 +0500

    parol generatisya asosiy qismi tugadi

--- src/generator/views.py
+++ src/generator/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -6,4 +8,25 @@ def home(request):
     return render(request,'generator/home.html')
 
 def about(request):
-    return HttpResponse('about page')
+    return render(request,'generator/about.html')
+
+
+def password(request):
+    password=''
+    chars=list('abcdefghijklmnopqrstuvwxyz')
+    print(chars)
+    print(list('abcdefghijklmnopqrstuvwxyz'.upper()))
+    length=int(request.GET.get('length'))
+
+    if request.GET.get('uppercase'):
+        chars.extend(list('abcdefghijklmnopqrstuvwxyz'.upper()))
+
+    if request.GET.get('special'):
+        chars.extend(list('!@#$%^&*()'))
+
+    if request.GET.get('numbers'):
+        chars.extend(list('0123456789'))
+
+    for i in range(length):
+        password+=random.choice(chars)
+    return render(request,'generator/password.html',{'password':password})
